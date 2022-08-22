0b0939aa45f42f7168301ce9be66ece6746f40b1
nhutphong <nhutphong@outlook.com>
2022-08-22 12:26:37 +0700

    basic

---
+++ contro-flow.js
@@ -0,0 +1,39 @@
+/*
+
+
+*/
+
+
+var time = new Date().getHours();
+var greeting;
+if (time < 10) {
+  greeting = "Good morning";
+} else if (time < 20) {
+  greeting = "Good day";
+} else {
+  greeting = "Good evening";
+}
+
+
+switch (new Date().getDay()) {
+  case 0:
+    day = "Sunday";
+    break; // phai co break trong moi case
+  case 1:
+    day = "Monday";
+    break;
+  case 2:
+     day = "Tuesday";
+    break;
+  case 3:
+    day = "Wednesday";
+    break;
+  case 4:
+    day = "Thursday";
+    break;
+  case 5:
+    day = "Friday";
+    break;
+  case 6:
+    day = "Saturday";
+}
