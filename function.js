5bf61c753049cb43a7f63840dadcc7d5b2692dfb
nhutphong <nhutphong@outlook.com>
2022-10-24 12:12:41 +0700

    modify

--- function.js
+++ function.js
@@ -1,4 +1,11 @@
 
+/*
+  arguments: 'la array chua args cua function'
+  arguments[0] = 'arg1'
+  arguments[1] = 'arg2'
+  arguments[2] = 'arg3'
+*/
+
 function Person(firstName, lastName) {
   var fstName = firstName;
   var lstName = lastName;
@@ -56,3 +63,57 @@ var hat = new Food('Cái mũ', 6);
  
 console.log(food);
 console.log(hat);
+
+
+
+/*
+apply(thisArg)
+apply(thisArg, argsArray)
+thisArg phai la obj || null || undefined
+*/
+var person1 = {firstName: 'Jon', lastName: 'Kuperman'};
+var person2 = {firstName: 'Kelly', lastName: 'King'};
+function say(greeting1, greeting2) {
+ console.log(greeting1 + ',' + greeting2 + ' ' + this.firstName + ' ' + this.lastName);
+}
+// fnName.call(argOne = this, two, three, four, ...) // da run
+say.call(person1, 'Hello', 'Good morning'); // => Hello,Good morning Jon Kuperman
+say.call(person2, 'Hello', 'Good morning'); // => Hello,Good morning Kelly King
+
+
+var person1 = {firstName: 'Jon', lastName: 'Kuperman'};
+var person2 = {firstName: 'Kelly', lastName: 'King'};
+function say(greeting0, greeting1) {
+ console.log(greeting0 + ',' + greeting1 + ' ' + this.firstName + ' ' + this.lastName);
+}
+// fnName.apply(argOne = this, [two, three, four, ...]) // da run
+say.apply(person1, ['Hello', 'Good moring']); // => Hello,Good moring Jon Kuperman
+say.apply(person2, ['Hello', 'Good moring']); // => Hello,Good moring Kelly King
+
+/*
+ giong nhu null muon method max of Math
+  Math.max(a1, a2, a3, ...)
+  null.max(array) //vi dung apply nen la array
+*/
+const numbers = [5, 6, 2, 3, 7];
+const max = Math.max.apply(null, numbers);
+console.log(max);
+// 7
+const min = Math.min.apply(null, numbers);
+console.log(min);
+// 2
+
+
+var person1 = {firstName: 'Jon', lastName: 'Kuperman'};
+var person2 = {firstName: 'Kelly', lastName: 'King'};
+function say(greeting0, greeting1) {
+ console.log(greeting0 + ',' + greeting1 + ' ' + this.firstName + ' ' + this.lastName);
+}
+
+// callback = fnName.bind(argOne = this, two, three, four, ...)
+// callback()
+var sayHelloJon = say.bind(person1, 'Hello', 'Good morning');
+var sayHelloKelly = say.bind(person2, 'Hello', 'Good morning');
+
+sayHelloJon(); // => Hello,Good morning Jon Kuperman
+sayHelloKelly(); // => Hello,Good morning Kelly King
